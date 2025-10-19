f9098c24a375218a3149457128f163ddf3a3e129
Daniel Parnell <me@danielparnell.com>
2025-10-19 15:24:23 +1100

    update dependencies, add `json` crate, and implement `ReclaimState` parsing from JSON

--- src/reclaim/mod.rs
+++ src/reclaim/mod.rs
@@ -1,6 +1,50 @@
+use std::str::FromStr;
+
 pub const DEFAULT_AWS_REGION: &str = "ap-southeast-2";
 pub const DEFAULT_AWS_IDENTITY_POOL: &str = "ap-southeast-2:e04c5d62-0c40-4eac-a343-27d5f76c4920";
 pub const DEFAULT_AWS_ENDPOINT: &str = "a254daig9zo2wn-ats.iot.ap-southeast-2.amazonaws.com";
+
+pub struct ReclaimState {
+    pub pump_active: bool,
+    pub case_temperature: f32,
+    pub water_temperature: f32,
+    pub outlet_temperature: f32,
+    pub inlet_temperature: f32,
+    pub discharge_temperature: f32,
+    pub suction: f32,
+    pub evaporator: f32,
+    pub ambient_temperature: f32,
+    pub compressor_speed: f32,
+    pub water_speed: f32,
+    pub fan_speed: f32,
+    pub power: f32,
+    pub current: f32,
+}
+
+impl FromStr for ReclaimState {
+    type Err = json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let json = json::parse(s)?;
+
+        Ok(ReclaimState {
+            pump_active: false,
+            case_temperature: 0.0,
+            water_temperature: 0.0,
+            outlet_temperature: 0.0,
+            inlet_temperature: 0.0,
+            discharge_temperature: 0.0,
+            suction: 0.0,
+            evaporator: 0.0,
+            ambient_temperature: 0.0,
+            compressor_speed: 0.0,
+            water_speed: 0.0,
+            fan_speed: 0.0,
+            power: 0.0,
+            current: 0.0,
+        })
+    }
+}
 
 pub fn validate_unique_id(id: &str) -> bool {
     // id is a 17 characters long integer
